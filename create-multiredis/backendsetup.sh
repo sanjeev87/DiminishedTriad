e2ed40c03de693b31d8cbe9c02249f983c78899f
sanjeev <sanjeev.jagannatharao@gmail.com>
2015-05-27 15:25:32 -0700

    adding scripts for testing

---
+++ create-multiredis/backendsetup.sh
@@ -0,0 +1,4 @@
+#clean logs, set up 6 beackend, start sentinel
+../create-multiredis/create-multiredis clean
+../create-multiredis/create-multiredis start
+../create-multiredis/create-multiredis sentinel
