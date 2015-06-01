1c83c11d84667b6abe1d03467af9fef4658ee575
Manindra Moharana <mkmoharana29@gmail.com>
2015-06-01 01:14:22 -0700

    updated tester

--- tester.sh
+++ tester.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-for y in {1..99}
-do
-for x in {a..z}
-do
-	let z=$(( ( RANDOM % 6 )  + 1 ))
-	echo "$z $x$y"
-	curl "http://localhost:5000$z/set?key=$x$y&value=$x$y"
-done
+max=26
+y=0
+while [  $((y)) -lt $((max)) ]; do
+    for x in {a..z}; do
+        let z=$(( ( RANDOM % 6 )  + 1 ))
+        curl "http://localhost:5000$z/set?key=$x$y&value=$x$y" > op.txt 2>&1 
+    done
+    w=$((y%5))
+    if [ $w -eq 0 ]
+    then
+        echo "key set ${y}%"
+    fi
+    y=$((y+1))
 done
 
-for y in {1..99}
-do
-for x in {a..z}
-do
-	let z=$(( ( RANDOM % 6 )  + 1 ))
-	echo "$z $x$y"
-	curl "http://localhost:5000$z/get?key=$x$y"
-	echo "\n"
+y=0
+while [  $((y)) -lt $((max)) ]; do
+    for x in {a..z}; do
+        let z=$(( ( RANDOM % 6 )  + 1 ))
+        # curl "http://localhost:5000$z/get?key=$x$y" > op.txt 2>&1
+        
+        op=$(curl "http://localhost:5000$z/get?key=$x$y" 2>er.txt)
+        # echo ".${x}${y}"
+        # echo "${op}" 
+        if [ "$op" != "\"$x$y\"" ]
+        then
+            echo "MISMATCH, got: $op expected: $x$y"
+        fi
+    done
+    w=$((y%5))
+    if [ $w -eq 0 ]
+    then
+        echo "key get ${y}%"
+    fi
+    y=$((y+1))
 done
-done
+
+echo "TEST OK."
